0b989055d0fbf7d1b4c42b1920a032e2f76b199d
Nathan Wright <nathanwrightbusiness@gmail.com>
2023-06-22 17:47:26 +0000

    arduino code for reading potentionmeter

---
+++ read_potentiometer/read_potentiometer.ino
@@ -0,0 +1,33 @@
+// /*
+//   AnalogReadSerial
+
+//   Reads an analog input on pin 0, prints the result to the Serial Monitor.
+//   Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+//   Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+//   This example code is in the public domain.
+
+//   https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
+// */
+#include <ros.h>
+#include <std_msgs/UInt32.h>
+
+ros::NodeHandle nh;
+
+std_msgs::UInt32 msg;
+ros::Publisher chatter("chatter", &msg);
+
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(chatter);
+}
+
+void loop()
+{
+  msg.data = analogRead(A0);
+  chatter.publish( &msg );
+  nh.spinOnce();
+  delay(100);
+}
