969d1a0e0367fe4aed4f6c640c3fe9cf5b46add2
nwright <nathanwrightbusiness@gmail.com>
2023-08-23 15:02:26 -0400

    added make plan and store location

--- stretch_seeing_eye/include/stretch_seeing_eye/WaypointGlobalPlanner.h
+++ stretch_seeing_eye/include/stretch_seeing_eye/WaypointGlobalPlanner.h
@@ -10,12 +10,14 @@
 #include <nav_msgs/Path.h>
 #include <pluginlib/class_list_macros.h>
 #include <ros/ros.h>
+#include <std_msgs/UInt32.h>
 #include <std_msgs/UInt8.h>
 
 #include <iostream>
 #include <vector>
 
 #include "Dijkstra.h"
+#include "stretch_seeing_eye/MakePath.h"
 #include "stretch_seeing_eye/WaypointDijkstra.h"
 
 using std::string;
@@ -36,15 +38,24 @@ class WaypointGlobalPlanner : public global_planner::GlobalPlanner {
     bool makePlan(const geometry_msgs::PoseStamped& start,
                   const geometry_msgs::PoseStamped& goal,
                   std::vector<geometry_msgs::PoseStamped>& plan);
+    // Custom functions
+    bool makePath(stretch_seeing_eye::MakePath::Request& req,
+                  stretch_seeing_eye::MakePath::Response& res);
     void waypointCallback(const stretch_seeing_eye::WaypointDijkstraPtr& msg);
-    size_t closestWaypoint(const geometry_msgs::PoseStamped& pose);
 
    private:
     ros::NodeHandle pnh;
     ros::Subscriber waypoint_sub;
+    ros::Subscriber current_waypoint_sub;
     ros::ServiceServer waypoint_goal_srv;
+    ros::ServiceServer waypoint_path_srv;
     std::vector<geometry_msgs::PoseStamped> waypoints;
+    int current_waypoint;
     Dijkstra dijkstra;
+
+    size_t closestWaypointPath(const geometry_msgs::PoseStamped& pose);
+    size_t closestWaypointDist(const geometry_msgs::PoseStamped& pose);
+    void currentWaypointCallback(const std_msgs::UInt32& msg);
 };
 
 double pathDistance(const std::vector<geometry_msgs::PoseStamped>& path);
