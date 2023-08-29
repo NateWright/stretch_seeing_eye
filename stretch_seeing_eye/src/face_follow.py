853a7b12cc78487d30768a92d58a08b1c1748139
Nate Wright <nathanwrightbusiness@gmail.com>
2023-08-29 15:26:24 +0000

    reverse x and y for angle

--- stretch_seeing_eye/src/face_follow.py
+++ stretch_seeing_eye/src/face_follow.py
@@ -23,7 +23,7 @@ class FollowClass():
         # rospy.logdebug("got message")
         if len(arr.people) < 1:
             return
-        angle = Math.atan2(arr.people[0].pos.y, arr.people[0].pos.x)
+        angle = Math.atan2(arr.people[0].pos.x, arr.people[0].pos.y)
         rospy.logdebug(arr.people[0].pos)
         if abs(angle) < 0.05:
             return
